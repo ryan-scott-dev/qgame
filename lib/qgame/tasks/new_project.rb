0536db26f34f5a77afe35e8eddff5d6f0b185c45
Ryan Scott <atthealma@gmail.com>
2013-05-24 19:33:55 +1000

    Improved the application creation so that it checks if the application name is valid, and camelizes it for  usage.

--- lib/qgame/tasks/new_project.rb
+++ lib/qgame/tasks/new_project.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'active_support/all'
 
 task :new, :args do |t, args|
   QGame::NewProjectTask.run(args)
@@ -7,16 +8,19 @@ end
 module QGame
   class NewProjectTask
     PROJECT_NAME_TOKEN = /\$PROJ_NAME/
+    RESERVED_NAMES = %w[application]
 
     def initialize(args)
       @project_name = args[0]
+
+      validate_project_name
     end
 
     def run
       create_from_skeleteon
       customise_created_skeleton
 
-      # git_clone('git://github.com/mruby/mruby.git', "#{@project_name}/build/mruby")
+      git_clone('git://github.com/mruby/mruby.git', "#{@project_name}/build/mruby")
     end
 
     def create_from_skeleteon
@@ -33,14 +37,43 @@ module QGame
 
         File.open(file) { |f| contents = f.read }
         # replace every occurance of the reserved word with our own
-        contents.gsub!(PROJECT_NAME_TOKEN, @project_name)
+        contents.gsub!(PROJECT_NAME_TOKEN, app_const)
         File.open(file, "w+") { |f| f.write(contents) }
       end
     
     end
 
+    def defined_app_const_base
+      QGame.respond_to?(:application) && defined?(QGame::Application) &&
+        QGame.application.is_a?(QGame::Application) && QGame.application.class.name.sub(/::Application$/, "")
+    end
+
+    alias :defined_app_const_base? :defined_app_const_base
+
+    def app_name
+      @app_name ||= (defined_app_const_base? ? defined_app_name : File.basename("#{@project_name}/")).tr(".", "_")
+    end
+
+    def defined_app_name
+      defined_app_const_base.underscore
+    end
+
+    def app_const
+      @app_const ||= defined_app_const_base || app_name.gsub(/\W/, '_').squeeze('_').camelize
+    end
+
     def git_clone(git_url, path)
       FileUtils.sh "git clone #{git_url} #{path}"
+    end
+    
+    def validate_project_name
+      if app_const =~ /^\d/
+        raise Error, "Invalid application name #{app_name}. Please give a name which does not start with numbers."
+      elsif RESERVED_NAMES.include?(app_name)
+        raise Error, "Invalid application name #{app_name}. Please give a name which does not match one of the reserved words."
+      elsif Object.const_defined?(app_const)
+        raise Error, "Invalid application name #{app_name}, constant #{app_const_base} is already in use. Please choose another application name."
+      end
     end
 
     def self.run(args)
