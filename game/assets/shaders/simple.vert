1933b8f8a8cb76ebf1f3997d655a0c42b4621254
Ryan Scott <atthealma@gmail.com>
2013-06-16 19:35:20 +1000

    Implemented the position for a sprite.

--- game/assets/shaders/simple.vert
+++ game/assets/shaders/simple.vert
@@ -6,6 +6,7 @@ in vec2 vertTexCoord;
 out vec2 fragTexCoord;
 
 uniform mat4 projection;
+uniform vec2 position;
 uniform vec2 scale;
 
 void main() {
@@ -14,7 +15,8 @@ void main() {
     mat4 world = mat4(1.0);
     world[0][0] = scale.x;
     world[1][1] = scale.y;
+    world[3][0] = position.x;
+    world[3][1] = position.y;
 
-    // does not alter the vertices at all
     gl_Position = projection * world * vec4(vert, 1);
 }
--- lib/qgame/sprite.rb
+++ lib/qgame/sprite.rb
@@ -5,9 +5,11 @@ module QGame
 
     def initialize(args = {})
       @texture = args[:texture]
-      @position = args[:position] || Vec2.new
+      @position = args[:position] || Vec2.new(1,1)
       @rotation = args[:rotation] || 0.0
       @scale = args[:scale] || Vec2.new(1)
+
+      @timestep = 0
     end
 
     def self.model
@@ -20,6 +22,10 @@ module QGame
     end
 
     def update
+      @timestep += 0.01
+      @position.x = 3 + Math.sin(@timestep)
+      @position.y = 2 + Math.cos(@timestep)
+
       QGame::RenderManager.submit(self)
     end
 
@@ -31,7 +37,7 @@ module QGame
       Sprite.shader.set_uniform('tex', 0)
       Sprite.shader.set_uniform('projection', Mat4.orthogonal_2d(0, 8, 0, 6, -1, 1))
 
-      # Sprite.shader.set_uniform('position', @position)
+      Sprite.shader.set_uniform('position', @position)
       # Sprite.shader.set_uniform('rotation', @rotation)
       Sprite.shader.set_uniform('scale', @scale)
 
