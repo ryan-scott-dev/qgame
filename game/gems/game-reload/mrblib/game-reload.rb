0354d41d9a379c4a6f6c542ce9ef22e16458e7e5
Ryan Scott <atthealma@gmail.com>
2013-09-08 22:24:15 +0800

    Implemented nested code reloading.

--- game/gems/game-reload/mrblib/game-reload.rb
+++ game/gems/game-reload/mrblib/game-reload.rb
@@ -1,16 +1,26 @@
 module Game
   def self.reload
+    reload_path('./game/lib')
+    
+    true
+  end
+
+  def self.reload_path(path)
     path_blacklist = ['.', '..', 'application.rb']
 
-    Dir.entries('./game/lib').each do |file|
+    Dir.entries(path).each do |file|
       next if path_blacklist.include?(file)
-      next unless file.include? '.rb'
+      
+      file_path = "#{path}/#{file}"
 
-      file_path = "./game/lib/#{file}"
-      puts file_path
-      self.reload_file(file_path)
+      if Dir.exists? file_path
+        reload_path(file_path)
+      else
+        next unless file.include? '.rb'
+
+        puts file_path
+        self.reload_file(file_path)
+      end
     end
-
-    true
   end
 end
