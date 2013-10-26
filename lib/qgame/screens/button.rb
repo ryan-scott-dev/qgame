375d8a7e8c257fb1bfb07ba4f11a669703582859
Ryan Scott <atthealma@gmail.com>
2013-10-26 11:15:04 +1100

    Added optional text to be overlaid for buttons.

--- lib/qgame/screens/button.rb
+++ lib/qgame/screens/button.rb
@@ -6,8 +6,27 @@ module QGame
 
       @texture_pressed = args[:texture_pressed]
       @mode = args[:mode] || :on_release
-
+      if args.has_key?(:text)
+        @text = Text.new(args[:text].merge({:parent => self}))
+        text_size = @text.size
+        @text_offset = Vec2.new(-text_size.x / 2, text_size.y / 2)
+        @text.position = self.position + @text_offset
+      end
       @on_pressed = block
+    end
+
+    def position=(newPosition)
+      super
+      @text.position = newPosition + @text_offset unless @text.nil?
+    end
+
+    def transparency
+      @absolute_transparency
+    end
+
+    def submit_render
+      super
+      @text.submit_render unless @text.nil?
     end
 
     def handle_mouse_down(event)
