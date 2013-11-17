98ac49e706eef424a06ca3667934747b8bd7255d
Ryan Scott <atthealma@gmail.com>
2013-11-17 21:49:13 +1100

    Made the enemy health indicator display their health.

--- game/game/lib/enemy.rb
+++ game/game/lib/enemy.rb
@@ -4,14 +4,22 @@ module TestGame
   class Enemy < WorldObject
     include QGame::EventHandler
 
+    attr_accessor :health
+
     def initialize(args = {})
       @direction = Vec3.new(0, 0, 1)
       
-      @health_indicator = WorldText.new(:text => "Hello World", :color => Color.red)
+      @health = args[:health] || 10
+      @health_indicator = WorldText.new(:text => @health.to_s, :color => Color.red)
       @health_indicator_offset = Vec3.new(0, 5, 0)
-
+      
       args[:scale] = Vec3.new(1, 2, 1)
       super(args)
+    end
+
+    def health=(val)
+      @health = val
+      @health_indicator.text = @health.to_s
     end
 
     def update
